c2501d6f3429dc65e12138e393633ee6a74bd98a
sojung <Sojung1214>
2021-04-11 14:45:55 +0900

    ep.07

--- app/src/main/java/com/example/test_application/MainActivity.java
+++ app/src/main/java/com/example/test_application/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.test_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText TextInputEditText_email, TextInputEditText_password;
+    RelativeLayout RelativeLayout_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login); //화면매칭
+
+        TextInputEditText_email = findViewById(R.id.TextInputEditText_email);
+        TextInputEditText_password = findViewById(R.id.TextInputEditText_password);
+        RelativeLayout_login = findViewById(R.id.RelativeLayout_login);
+
     }
 }
